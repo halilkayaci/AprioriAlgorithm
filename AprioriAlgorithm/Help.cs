0f31b3b19ee33244535fcf198766706a1342430e
kaptann <kaptan.rar@gmail.com>
2019-05-20 01:30:48 +0300

    Add comment

--- AprioriAlgorithm/Help.cs
+++ AprioriAlgorithm/Help.cs
@@ -26,6 +26,7 @@ namespace AprioriAlgorithm
         private void rbtn_Step1_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 1 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step1;
             lbl_Step1.ForeColor = Color.FromArgb(0, 128, 64);
         }
@@ -33,6 +34,7 @@ namespace AprioriAlgorithm
         private void rbtn_Step2_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 2 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step2;
             lbl_Step2.ForeColor = Color.FromArgb(0, 128, 64);
         }
@@ -40,13 +42,15 @@ namespace AprioriAlgorithm
         private void rbtn_Step3_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
-            image_Help.Image = Properties.Resources.Step3;
+            // adım 3 resmi gösterilip label rengi değiştiriliyor.
+	    image_Help.Image = Properties.Resources.Step3;
             lbl_Step3.ForeColor = Color.FromArgb(0, 128, 64);
         }
 
         private void rbtn_Step4_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 4 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step4;
             lbl_Step4.ForeColor = Color.FromArgb(0, 128, 64);
         }
@@ -54,6 +58,7 @@ namespace AprioriAlgorithm
         private void rbtn_Step5_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 5 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step5;
             lbl_Step5.ForeColor = Color.FromArgb(0, 128, 64);
         }
@@ -61,6 +66,7 @@ namespace AprioriAlgorithm
         private void rbtn_Step6_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 6 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step6;
             lbl_Step6.ForeColor = Color.FromArgb(0, 128, 64);
         }
@@ -68,6 +74,7 @@ namespace AprioriAlgorithm
         private void rbtn_Step7_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 7 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step7;
             lbl_Step7.ForeColor = Color.FromArgb(0, 128, 64);
         }
@@ -75,6 +82,7 @@ namespace AprioriAlgorithm
         private void rbtn_Step8_Click(object sender, EventArgs e)
         {
             changeAllLabelColor();
+	    // adım 8 resmi gösterilip label rengi değiştiriliyor.
             image_Help.Image = Properties.Resources.Step8;
             lbl_Step8.ForeColor = Color.FromArgb(0, 128, 64);
         }
